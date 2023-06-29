a2d04e2e5d94aa38eb192a4dcfd6973d0631317c
Nolen Johnson <johnsonnolen@gmail.com>
2023-06-29 15:53:31 +0200

    odroidn2l: Initial Commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_odroid2nl.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_odroidn2l-eng \
+    lineage_odroidn2l-user \
+    lineage_odroidn2l-userdebug
